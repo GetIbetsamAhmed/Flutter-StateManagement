03730ab41dbae1a1de2ce738df14a4c7f57888bc
Ibtesam Ahmed ( SK ) <51031949+SkyLineProgrammers@users.noreply.github.com>
2022-09-08 15:53:10 +0500

    review changes

--- get_x/lib/controllers/cart_controller.dart
+++ get_x/lib/controllers/cart_controller.dart
@@ -5,6 +5,7 @@ import '../models/product.dart';
 class CartController extends GetxController {
   var cartItems = <Product>[].obs;
   int get count => cartItems.length;
+  // Fold is used to looping
   double get totalPrice => cartItems.fold(0, (sum, item) => sum + item.price);
 
   addToCart(Product product) {
