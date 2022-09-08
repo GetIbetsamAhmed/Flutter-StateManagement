f300f28e6dfa9ff63c4d9ad7aad4f78284f7e1c3
Ibtesam Ahmed ( SK ) <51031949+SkyLineProgrammers@users.noreply.github.com>
2022-09-08 15:28:26 +0500

    implement getX state management with basic example

---
+++ get_x/lib/controllers/cart_controller.dart
@@ -0,0 +1,13 @@
+import 'package:get/state_manager.dart';
+
+import '../models/product.dart';
+
+class CartController extends GetxController {
+  var cartItems = <Product>[].obs;
+  int get count => cartItems.length;
+  double get totalPrice => cartItems.fold(0, (sum, item) => sum + item.price);
+
+  addToCart(Product product) {
+    cartItems.add(product);
+  }
+}
